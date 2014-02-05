23422c8281110f7be8ab0919a661c3cf253df29b
Ryan <ry@Ryans-MacBook-Pro.local>
2014-02-05 19:47:18 +0100

    Added PHP file

---
+++ feedback.php
@@ -0,0 +1,26 @@
+<?php
+ 
+//Change the e-mail address (Line 24) & place this file on your webserver.
+//For example: http://www.yourdomain.com/feedback.php
+//Then post data to it.
+ 
+// Retrieve variables
+$feedbackResult = $_POST['feedbackResult'];
+$feedbackMessage = $_POST['feedbackMessage'];
+$feedbackSender = $_POST['feedbackSender'];
+$feedbackIsPRO = $_POST['feedbackIsPRO'];
+$feedbackVersion = $_POST['feedbackVersion'];
+ 
+// Trim 'em
+$feedbackResult = trim($feedbackResult);
+$feedbackMessage = trim($feedbackMessage);
+$feedbackSender = trim($feedbackSender);
+$feedbackIsPRO = trim($feedbackIsPRO);
+$feedbackVersion  = trim($feedbackVersion);
+ 
+// Prepare the subject line and message
+$subject = "In-app Feedback"; 
+$message = "Rating: $feedbackResult \r Sender: $feedbackSender \r Message: $feedbackMessage \r Pro User: $feedbackIsPRO \r Version: $feedbackVersion";
+  mail('contact@your-email-here.com', $subject,$message) or "bad";
+ 
+?>
